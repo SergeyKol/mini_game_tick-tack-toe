51c8150eced4ac4ad01c64a9c7b1c528fb5c5a59
SergeyKol <koseva@yandex.ru>
2016-06-28 10:40:35 +0400

    new map

--- tick-tack-toe/tick-tack-toe/Program.cs
+++ tick-tack-toe/tick-tack-toe/Program.cs
@@ -10,26 +10,29 @@ namespace tick_tack_toe
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            //Point p1 = new Point(1, 3, '*');
+            //p1.Draw();
 
-            HorizontalLine upLine = new HorizontalLine(1, 5, 1, '+');
-            HorizontalLine DownLine = new HorizontalLine(1, 5, 5, '+');
-            VerticalLine leftLine = new VerticalLine(1,5,1,'+');
-            VerticalLine rightLine = new VerticalLine(1, 5, 5, '+');
+            //HorizontalLine upLine = new HorizontalLine(1, 5, 1, '+');
+            //HorizontalLine DownLine = new HorizontalLine(1, 5, 5, '+');
+            //VerticalLine leftLine = new VerticalLine(1,5,1,'+');
+            //VerticalLine rightLine = new VerticalLine(1, 5, 5, '+');
 
-            upLine.Drow();
-            DownLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
+            //upLine.Drow();
+            //DownLine.Drow();
+            //leftLine.Drow();
+            //rightLine.Drow();
 
-            Map map = new Map(23, 16);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            map.Draw();
-            Console.ForegroundColor = ConsoleColor.White;
+            //Map map = new Map(6, 6);
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //map.Draw();
+            //Console.ForegroundColor = ConsoleColor.White;
 
+            //Tetragon tetragon = new Tetragon(0,6,0,6);
+            //mapList.Add();
+            //tetragon.Draw();
             //Console.WriteLine("END");
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
