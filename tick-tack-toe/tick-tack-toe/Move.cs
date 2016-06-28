2f9348ec1401bf55ca3b39e851a1e6868125d2df
SergeyKol <koseva@yandex.ru>
2016-06-28 12:14:07 +0400

    add function move user

---
+++ tick-tack-toe/tick-tack-toe/Move.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tick_tack_toe
+{
+    class Move
+    {
+        public static int get_move()
+        {
+            //подсказка пользователю, куда можно поставить свой ход.
+            Console.WriteLine("Кнопки управления ходами: \n");
+            Console.WriteLine("-7-|-8-|-9-");
+            Console.WriteLine("-4-|-5-|-6-");
+            Console.WriteLine("-1-|-2-|-3-");
+            Console.WriteLine("\n");
+
+            //вывод текущего состояния доски
+            Console.WriteLine("Текущие состояние игрового поля: \n");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine("---|---|---");
+
+            //ожидание и обработка ввода клавиши с клавиатуры
+            Console.WriteLine("\nВаш ход:");         
+            //ConsoleKeyInfo move = Console.ReadKey();
+            int move = Convert.ToInt32(Console.ReadLine());
+
+            while (move > 9 || move < 1)
+            {
+                Console.WriteLine("\nУкажите число от 1 до 9");
+                Console.WriteLine("\nВаш ход:");
+                move = Convert.ToInt32(Console.ReadLine());
+            }
+            return move;
+        }
+    }
+}
--- tick-tack-toe/tick-tack-toe/Program.cs
+++ tick-tack-toe/tick-tack-toe/Program.cs
@@ -10,29 +10,10 @@ namespace tick_tack_toe
     {
         static void Main(string[] args)
         {
-            //Point p1 = new Point(1, 3, '*');
-            //p1.Draw();
-
-            //HorizontalLine upLine = new HorizontalLine(1, 5, 1, '+');
-            //HorizontalLine DownLine = new HorizontalLine(1, 5, 5, '+');
-            //VerticalLine leftLine = new VerticalLine(1,5,1,'+');
-            //VerticalLine rightLine = new VerticalLine(1, 5, 5, '+');
-
-            //upLine.Drow();
-            //DownLine.Drow();
-            //leftLine.Drow();
-            //rightLine.Drow();
-
-            //Map map = new Map(6, 6);
-            //Console.ForegroundColor = ConsoleColor.Yellow;
-            //map.Draw();
-            //Console.ForegroundColor = ConsoleColor.White;
-
-            //Tetragon tetragon = new Tetragon(0,6,0,6);
-            //mapList.Add();
-            //tetragon.Draw();
-            //Console.WriteLine("END");
-            //Console.ReadLine();
+            int move = Move.get_move();
+            //ConsoleKeyInfo move1 = Console.ReadKey();
+            Console.WriteLine("\nЗначение которое вы ввели "+ move);
+            Console.ReadLine();
         }
     }
 }
